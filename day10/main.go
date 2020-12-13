c5293125920c75fac8a21b831fd149b2b8756661
Yan Hao <435390541@qq.com>
2020-12-13 15:10:43 +0800

    solution for day10

---
+++ day10/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func solution() int {
+	result := make([]int, 0)
+	result = append(result, 0)
+
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			continue
+		}
+
+		for _, line := range strings.Split(string(data), "\n"){
+			number, ok := strconv.Atoi(line)
+			if ok == nil {
+				result = append(result, number)
+			}
+		}
+	}
+
+	sort.Ints(result)
+	//onePlusNumber := 1
+	//threePlusNumber := 1
+
+	//for i := 1; i < len(result); i++ {
+	//	if result[i] - result[i - 1] == 1 {
+	//		onePlusNumber++
+	//	} else if result[i] - result[i - 1] == 3{
+	//		threePlusNumber++
+	//	}
+	//}
+
+	mutliNumber := 1
+
+	for i := 0; i < len(result) - 1; i++ {
+		temp := make([]int, 0)
+
+		for i + 1 < len(result) && result[i + 1] - result[i] == 1 {
+			temp = append(temp, result[i])
+			i++
+		}
+		if len(temp) != 0{
+			temp = append(temp, result[i])
+		} else{
+			temp = append(temp, result[i])
+		}
+
+		if len(temp) == 3 {
+			mutliNumber *= 2
+		} else if len(temp) == 4 {
+			mutliNumber *= 4
+		} else if len(temp) == 5 {
+			mutliNumber *= 7
+		}
+		//fmt.Println(temp)
+	}
+
+	return mutliNumber
+	//return onePlusNumber * threePlusNumber
+}
+
+func main(){
+	answer := solution()
+	fmt.Println(answer)
+}
+
