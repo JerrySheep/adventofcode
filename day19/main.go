cc1e75df96590f4976cb3d724dee2f8ce5e18fba
Yan Hao <435390541@qq.com>
2020-12-30 19:58:18 +0800

    solution for day19 part-2 (copy from https://github.com/lizthegrey/adventofcode)

--- day19/main.go
+++ day19/main.go
@@ -1,3 +1,4 @@
+//copy from https://github.com/lizthegrey/adventofcode/blob/main/2020/day19.go
 package main
 
 import (
@@ -8,75 +9,45 @@ import (
 	"strings"
 )
 
-func storeMessages(ruleKeyValue map[int]string, store []string, index int) []string{
-	rule := ruleKeyValue[index]
-	ruleNumbers := strings.Split(rule, " ")
-	if ruleNumbers[0] == "\"a\"" {
-		for i := 0; i < len(store); i++ {
-			store[i] += "a"
+type Rule struct {
+	Literal string
+	Children [][]int
+}
+
+type RuleSet map[int]Rule
+
+func match(rules RuleSet, ruleNo int, s string) []int {
+	rule := rules[ruleNo]
+	if len(rule.Children) == 0 {
+		if len(s) < len(rule.Literal) {
+			return nil
 		}
-		return store
-	} else if ruleNumbers[0] == "\"b\"" {
-		for i := 0; i < len(store); i++ {
-			store[i] += "b"
-		}
-		return store
-	} else {
-		if !strings.Contains(rule, "|") {
-			ruleOnly := make([]int, 0)
-			count := 0
-			for count < len(ruleNumbers) {
-				number, _ := strconv.Atoi(ruleNumbers[count])
-				ruleOnly = append(ruleOnly, number)
-				count++
-			}
-
-			for i := 0; i < len(ruleOnly); i++ {
-				store = storeMessages(ruleKeyValue, store, ruleOnly[i])
-			}
-		} else {
-			ruleOne := make([]int, 0)
-			ruleTwo := make([]int, 0)
-			count := 0
-			for ruleNumbers[count] != "|" {
-				number, _ := strconv.Atoi(ruleNumbers[count])
-				ruleOne = append(ruleOne, number)
-				count++
-			}
-			count++
-
-			for count < len(ruleNumbers) {
-				number, _ := strconv.Atoi(ruleNumbers[count])
-				ruleTwo = append(ruleTwo, number)
-				count++
-			}
-
-			storeLeft := make([]string, len(store))
-			storeRight := make([]string, len(store))
-			copy(storeLeft, store)
-			copy(storeRight, store)
-
-			for i := 0; i < len(ruleOne); i++ {
-				storeLeft = storeMessages(ruleKeyValue, storeLeft, ruleOne[i])
-			}
-			for i := 0; i < len(ruleTwo); i++ {
-				storeRight = storeMessages(ruleKeyValue, storeRight, ruleTwo[i])
-			}
-
-			store = store[0:0]
-			for i := 0; i < len(storeLeft); i++ {
-				store = append(store, storeLeft[i])
-			}
-			for i := 0; i < len(storeRight); i++ {
-				store = append(store, storeRight[i])
-			}
+		if s[:len(rule.Literal)] == rule.Literal {
+			return []int{len(rule.Literal)}
 		}
 	}
 
-	return store
+	matchedChars := make([]int, 0)
+	for _, child := range rule.Children {
+		potentialMatches := []int{0}
+		for _, singleChild := range child {
+			newPotentialMatches := make([]int, 0)
+			for _, matchLength := range potentialMatches {
+				matches := match(rules, singleChild, s[matchLength:len(s)])
+
+				for _, value := range matches {
+					newPotentialMatches = append(newPotentialMatches, value + matchLength)
+				}
+			}
+			potentialMatches = newPotentialMatches
+		}
+		matchedChars = append(matchedChars, potentialMatches...)
+	}
+
+	return matchedChars
 }
 
-func solution() int {
+func solution() int{
 	result := make([]string, 0)
 
 	for _, filename := range os.Args[1:] {
@@ -91,52 +62,69 @@ func solution() int {
 		}
 	}
 
-	rules := make([]string, 0)
-	candidates := make([]string, 0)
+	result = result[:len(result) - 1]
 
-	index := 0
-	for result[index] != "" {
-		rules = append(rules, result[index])
-		index++
+	rules := make(RuleSet)
+	messages := make([]string, 0)
+	judge := false
+
+	for _, s := range result {
+		if s == "" {
+			judge = true
+			continue
+		}
+		if judge {
+			messages = append(messages, s)
+			continue
+		}
+
+		index := strings.Index(s, ":")
+		ruleNo, err := strconv.Atoi(s[0:index])
+		if err != nil {
+			fmt.Printf("Failed to parse %s\n", s)
+			break
+		}
+
+		rule := Rule{}
+		if s[index + 2] == '"' {
+			rule.Literal = s[index + 3 : len(s) - 1]
+		} else {
+			matches := strings.Split(s[index + 2 : len(s)], " | ")
+			for _, m := range matches {
+				elements := strings.Split(m, " ")
+				group := make([]int, 0)
+				for _, ele := range elements {
+					child, err := strconv.Atoi(ele)
+					if err != nil {
+						fmt.Printf("Failed to parse %s\n", s)
+						break
+					}
+					group = append(group, child)
+				}
+				rule.Children = append(rule.Children, group)
+			}
+		}
+		rules[ruleNo] = rule
 	}
 
-	ruleKeyValue := make(map[int]string, 0)
-	for i := 0; i < len(rules); i++ {
-		sentence := strings.Split(rules[i], ": ")
-		number, _ := strconv.Atoi(sentence[0])
-		ruleKeyValue[number] = sentence[1]
-	}
+	rules[8] = Rule{Children: [][]int{{42}, {42, 8}}}
+	rules[11] = Rule{Children: [][]int{{42, 31}, {42, 11, 31}}}
 
-	index++
-	for result[index] != "" {
-		candidates = append(candidates, result[index])
-		index++
-	}
-
-	message := ""
-	store := make([]string, 0)
-	store = append(store, message)
-
-	store = storeMessages(ruleKeyValue, store, 0)
-
-	keyValue := make(map[string]int, 0)
-
-	for i := 0; i < len(store); i++ {
-		keyValue[store[i]] = 1
-	}
-
-	validNumber := 0
-	for i := 0; i < len(candidates); i++ {
-		if _, ok := keyValue[candidates[i]]; ok {
-			validNumber++
+	matches := 0
+	for _, m := range messages {
+		symbolMatches := match(rules, 0, m)
+		for _, times := range symbolMatches {
+			if times == len(m){
+				matches++
+				break
+			}
 		}
 	}
 
-	return validNumber
+	return matches
 }
 
 func main(){
 	answer := solution()
 	fmt.Println(answer)
 }
-
